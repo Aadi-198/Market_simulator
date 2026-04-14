2196ccae4f73474f545f7608381c09b2f0ae113f
Pro <aadishresth@gmail.com>
2026-04-14 14:16:45 +0530

    Added a variable latency effect

--- MyFiles/Market.py
+++ MyFiles/Market.py
@@ -1,9 +1,11 @@
 # Make a market simulator
 
 # Import required modules and libraries
+import time
 import random
 
 seps = "-"*100                  #Separators for better visibility of code
+wait = [0.1, 0.2, 0.3, 0.4]     #wait as a variable to slow down the text and make it feel alive
 
 #Define a function to run the simulation
 def market():
@@ -18,6 +20,7 @@ def market():
     # Make a loop that shows the user its staus before making any changes
     simulating = True
     while simulating == True:
+        time.sleep(random.choice(wait))
         print(f"\n {seps} \n")
         print(f"Company name : {company}")
         print(f"Price of stock : {price}")
@@ -28,33 +31,61 @@ def market():
 
         #Make a variable that allows the user to control
         action = input(f"What would you like to do? (B)uy, (S)ell, (W)ait or (Q)uit. \n").strip().lower()
+        time.sleep(random.choice(wait))
         if action in ("b", "1", "buy"):       #leave multiple options so if user type b or buy it works
+            time.sleep(random.choice(wait))
             print("Purchase in progress ...")
+            time.sleep(random.choice(wait))
             buy = int(input("How many shares you want to purchase? \n"))
             total_cost = buy * price
             if wallet >= total_cost and stocks_available >= buy:
                 wallet -= total_cost
                 share_held += buy
                 stocks_available -= buy
-                print("Succesfully bought the shares \n")
+                time.sleep(random.choice(wait))
+                print("Succesfully bought the shares! \n")
             elif wallet < total_cost:
-                print("You don't have enough money /n")
+                time.sleep(random.choice(wait))
+                print("You don't have enough money! /n")
             elif stocks_available < buy:
+                time.sleep(random.choice(wait))
                 print(f"{buy} are not available. Try selecting a lower amount. \n")
             else:                       #Create safety mechanism
+                time.sleep(random.choice(wait))
                 print("Error")
-            
+            time.sleep(random.choice(wait))
         elif action in ("s", "2", "sell"):
-            print()
+            time.sleep(random.choice(wait))
+            print("Selling in progress ...")
+            time.sleep(random.choice(wait))
+            sell = int(input("How many shares you want to sell? \n"))
+            total_profit = sell * price
+            if share_held >= sell:
+                wallet += total_profit
+                share_held -= sell
+                stocks_available += sell
+                time.sleep(random.choice(wait))
+                print("Succesfully sold the shares! \n")
+            elif share_held < sell:
+                time.sleep(random.choice(wait))
+                print("You don't have enough shares! /n")
+            else:                       #Create safety mechanism
+                time.sleep(random.choice(wait))
+                print("Error")
+            time.sleep(random.choice(wait))
         elif action in ("w", "3", "wait"):
+            time.sleep(random.choice(wait))
             print()
         elif action in ("q", "4", "quit"):
+            time.sleep(random.choice(wait))
             confirmation = input("Are you sure you want to exit the simlation? \n")
             if confirmation in ("y", "yes"):
+                time.sleep(random.choice(wait))
                 print("Closed simulation")
                 simulating = False
             else:
                 continue
+            time.sleep(random.choice(wait))
         else:
             print("Error")      #make a safety mechanism
 
