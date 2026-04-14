1f65d011639ef3b1fe079358f5f55305a2c06b97
Pro <aadishresth@gmail.com>
2026-04-14 13:19:52 +0530

    Defined the basic structure of program.

---
+++ MyFiles/Market.py
@@ -0,0 +1,22 @@
+# Make a market simulator
+
+# Import required modules and libraries
+import random
+
+seps = "-"*100              #Separators for better visibility of code
+
+#Define a function to run the simulation
+def market():
+    print(seps)
+
+    # Make required variables
+    company = "Mango-Slop"      #Name of the company
+    price = 100.00              #Price of each stock
+    wallet = 1000.00            #Money you have
+    share_held = 0              #No. if shares u have
+    stocks_available = 1000     #No. of stocks available
+
+    # Make a loop that shows the user its staus before making any changes
+
+    print(seps)
+market()
